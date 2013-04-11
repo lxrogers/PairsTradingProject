9de3545d68dfdcb7b8b37ca8a399addaa04250fc
David Khavari <dkhavari@gmail.com>
2013-04-11 13:23:56 -0700

    Started building with the Stanford ACM libraries.

---
+++ src/algorithms/DataProcessing.java
@@ -0,0 +1,21 @@
+/*
+ * File: DataProcessing.java
+ * -------------------------
+ * This file pulls in stock data in .csv form and tokenizes it,
+ * putting the prices into an array of daily opens.
+ */
+
+/* Package Designation */
+package algorithms;
+
+/* Imports */
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import acm.program.*;
+
+public class DataProcessing extends ConsoleProgram {
+
+}
